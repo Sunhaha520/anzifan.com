91be842b0c6c95ea6ad2c74de2895ace4bf7b27d
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2023-07-13 16:17:03 +0000

    Pending changes exported from your codespace

--- lib/notion.ts
+++ lib/notion.ts
@@ -2,7 +2,7 @@ import { Client } from '@notionhq/client'
 import { Post } from './types'
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
-const databaseId = process.env.NOTION_DATABASE_ID || '7fddb522451e4ac68922d1515da1f5f4'
+const databaseId = process.env.NOTION_DATABASE_ID || '0442ceadc04045a7b505eecc6659f8f7'
 
 // export const getBasicDBInfo = async () => {
 //     const dbQuery: any = {
