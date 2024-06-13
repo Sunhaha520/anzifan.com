278fcda414d289bdc55bc06eddb6760bcf426658
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-06-13 19:51:54 +0800

    Update devices.tsx

--- pages/devices.tsx
+++ pages/devices.tsx
@@ -28,17 +28,17 @@ const devices: DeviceType[] = [
     // 添加更多设备信息
 ]
 
-const DeviceCard: FC<DeviceType> = device => {
+const DeviceCard: FC<DeviceType> = ({ name, img, description }) => {
     return (
-        <div className="flex items-center justify-center transition duration-200 ease-in-out transform bg-white shadow-lg h-55 lg:h-58 rounded-3xl hover:scale-105">
+        <div className="flex items-center justify-center transition duration-200 ease-in-out transform bg-white shadow-lg h-55 lg:h-58 rounded-3xl hover:scale-105 dark:bg-true-gray-900">
             <div className="flex flex-col items-center justify-between h-full p-5">
-                <div className="w-20 h-20 rounded-full bg-true-gray-200 relative overflow-hidden">
-                    <Image layout='fill' objectFit="cover" src={device.img} alt={device.name} />
+                <div className="w-20 h-20 bg-true-gray-200 relative overflow-hidden rounded-md dark:bg-true-gray-600">
+                    <Image layout='fill' objectFit="cover" src={img} alt={name} className="object-cover" />
                 </div>
                 <div className="pb-2 text-center">
-                    <p className="leading-4">{device.name}</p>
-                    <div className="mt-1 text-sm text-true-gray-400">
-                        {device.description}
+                    <p className="leading-4 dark:text-white">{name}</p>
+                    <div className="mt-1 text-sm text-true-gray-400 dark:text-true-gray-300">
+                        {description}
                     </div>
                 </div>
             </div>
@@ -50,7 +50,7 @@ const Devices: NextPage = () => {
 
     return (
         <ListLayout>
-            <h1 className="mb-4 text-2xl font-bold md:text-3xl lg:mb-8">我的数码设备</h1>
+            <h1 className="mb-4 text-2xl font-bold md:text-3xl lg:mb-8 dark:text-white">我的数码设备</h1>
             <div className="grid grid-cols-2 gap-4 my-6 md:grid-cols-4 lg:grid-cols-5">
                 {devices.map((device: DeviceType) => <DeviceCard key={device.name} {...device} />)}
             </div>
