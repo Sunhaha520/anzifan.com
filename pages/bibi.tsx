6b2dc38299bdfb0f148e7ad1b8f217cd6a06e5e1
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-06-14 09:36:38 +0800

    Update bibi.tsx

--- pages/bibi.tsx
+++ pages/bibi.tsx
@@ -21,15 +21,17 @@ const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 const MemoCard: FC<Memos> = (memo) => {
   return (
-    <div className="flex items-start p-4 transition duration-200 ease-in-out transform bg-white shadow-lg rounded-3xl hover:scale-105" dark="bg-true-gray-900">
-      <Image src="https://cdn.dribbble.com/userupload/15096090/file/original-a79a16aa0e2a9f34c4521da09f7cc4d0.webp" alt="可乐君" width={40} height={40} className="rounded-full" />
-      <div className="ml-4">
-        <div className="flex items-center justify-between">
-          <span className="font-bold">可乐君</span>
-          <time className="text-sm text-true-gray-400" dark="text-true-gray-500">{dayjs.unix(memo.createdTs).fromNow()}</time>
+    <div className="flex flex-col items-start p-4 transition duration-200 ease-in-out transform bg-white shadow-lg rounded-3xl hover:scale-105 dark:bg-true-gray-900">
+      <div className="flex items-center mb-4">
+        <Image src="https://cdn.dribbble.com/userupload/15096090/file/original-a79a16aa0e2a9f34c4521da09f7cc4d0.webp" alt="可乐君" width={40} height={40} className="rounded-full" />
+        <div className="ml-4">
+          <div className="flex items-center justify-between">
+            <span className="font-bold">可乐君</span>
+            <time className="ml-2 text-sm text-true-gray-400 dark:text-true-gray-500">{dayjs.unix(memo.createdTs).fromNow()}</time>
+          </div>
         </div>
-        <div className="mt-2 text-true-gray-800" dark="text-true-gray-300" dangerouslySetInnerHTML={{ __html: memo.content }}></div>
       </div>
+      <div className="text-true-gray-800 dark:text-true-gray-300" dangerouslySetInnerHTML={{ __html: memo.content }}></div>
     </div>
   );
 };
@@ -37,13 +39,13 @@ const MemoCard: FC<Memos> = (memo) => {
 const Bibi: NextPage = () => {
   const { data, error } = useSWR('https://tgapi.xiaoayu.eu.org/?tag=SFCN&limit=10&type=memos', fetcher);
 
-  if (error) return <div>加载失败</div>;
-  if (!data) return <div>加载中...</div>;
+  if (error) return <div className="flex items-center justify-center h-screen text-xl font-semibold text-red-600">加载失败</div>;
+  if (!data) return <div className="flex items-center justify-center h-screen text-xl font-semibold text-blue-600">加载中...</div>;
 
   return (
     <ListLayout>
       <h1 className="mb-4 text-2xl font-bold md:text-3xl lg:mb-8">说说✨</h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="space-y-4">
         {data.map((memo: Memos) => <MemoCard key={memo.id} {...memo} />)}
       </div>
     </ListLayout>
