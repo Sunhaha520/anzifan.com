8413bb455fcdb1d6e77a8c547f0ed18fdefae2c5
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-09-02 23:48:48 +0800

    Update friends.ts

--- config/friends.ts
+++ config/friends.ts
@@ -5,14 +5,19 @@ export type FriendType = {
 }
 
 export const friends: FriendType[] = [
-    {
+  {
     url: 'https://www.xiaoayu.ren',
     name: '阿宇の小站',
     img: 'https://cdn.jsdelivr.net/gh/Sunhaha520/picx-images-hosting@master/portrait.5n7ee6bgk7k0.webp',
   },
   {
+    url: 'https://think.xiaoayu.ren',
+    name: '可乐君的学术',
+    img: 'https://cdn.jsdelivr.net/gh/Sunhaha520/picx-images-hosting@master/portrait.5n7ee6bgk7k0.webp',
+  },
+  {
     url: 'https://blog.xiaoayu.ren',
-    name: '奶昔小站',
+    name: '奶昔小站(女友)',
     img: 'https://cdn.jsdelivr.net/gh/Sunhaha520/picx-images-hosting@master/20240111/imgbin_despicable-me-minion-rush-computer-icons-minions-png.3j2k0it4dsg0.webp',
   },
   {
@@ -29,5 +34,10 @@ export const friends: FriendType[] = [
     url: 'https://blog.zhheo.com/',
     name: '张洪Heo',
     img: 'https://img.zhheo.com/i/2022/08/19/62ff32fa28da1.png',
+  },
+  {
+    url: 'https://www.xalaok.top/',
+    name: 'Naive Koala 🐨',
+    img: 'https://cravatar.cn/avatar/44e7a197aa88dcad4764a1c0ea508d8c',
   },
 ]
