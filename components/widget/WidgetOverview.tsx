3ae34d1066e9f41bd9c224a238fc132975568fe1
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-09-03 18:26:16 +0800

    Update WidgetOverview.tsx

--- components/widget/WidgetOverview.tsx
+++ components/widget/WidgetOverview.tsx
@@ -7,15 +7,12 @@ import {
     LinearScale,
     PointElement,
     LineElement,
-
     Title,
-    // Tooltip,
     Legend,
     BarElement,
 } from 'chart.js';
 import { Line, Bar, Scatter } from 'react-chartjs-2';
 import { Colors } from "../../lib/colors"
-// TODO: gradient is too big
 import gradient from "chartjs-plugin-gradient"
 import { Tooltip } from "../utility/Tooltip"
 import { GetStaticProps } from "next";
@@ -23,6 +20,8 @@ import { getDatabase } from "../../lib/notion";
 import { Post } from "../../lib/types";
 import useSWRImmutable from 'swr/immutable';
 import { useTheme } from "next-themes";
+import requests from 'requests';
+import time from 'time';
 
 ChartJS.register(
     CategoryScale,
@@ -31,11 +30,26 @@ ChartJS.register(
     LineElement,
     BarElement,
     gradient
-    // Tooltip,
-    // Legend
 );
 
 const fetcher = (url: RequestInfo) => fetch(url).then((res) => res.json());
+
+const fetchVisitsData = async () => {
+    const url = "https://status.xiaoayu.ren/api/websites/f16983ff-10ab-49eb-a1ce-a5fca1ff70ba/stats";
+    const current_time_ms = int(time.time() * 1000);
+    const params = {
+        "startAt": "1656679719687",
+        "endAt": current_time_ms
+    };
+    const headers = {
+        "Accept": "application/json",
+        "Authorization": "Bearer OB0/RiJwtrc52+Nv1rHpi3SPcg2gtR0tfIPOh3zw3Fr7sDd4b/lWPu71cBKpPbRPAXjUw7ddVCpjGZjZCs94tAs3DKwufhvSLrcnExLLQYzID3Tnfaeez5t8clkzpi9pyTbGThBWUH/sHi6gJnXaGtzU+Nz65vtvBqpXGZcV1L1TzZZQpIhGgFTcmtcnQ9PoozXYwoyL5dOXrrRVGDED60KaSz3zZFYhJYhi11TZrnm6PGHhIb689f8QcJQYt3Fj1cKJ78GPfQR76/ZfD5mSqfJNMfaTDCxVZIz1uewitiG5xDbtNXhshS131S0By6dx9hHAdCqKfYVqMzJK+Nkw79jZmQXNPbKy3g=="
+    };
+
+    const response = await requests.get(url, headers=headers, params=params);
+    const data = response.json();
+    return data.visits.value;
+};
 
 export const WidgetOverViewSmall: FC<{ posts: Post[], }> = ({ posts }) => {
     const tagsMap = posts.map(p => ({ tags: p.tags, date: p.updateDate }))
@@ -46,14 +60,18 @@ export const WidgetOverViewSmall: FC<{ posts: Post[], }> = ({ posts }) => {
         count
     );
 
+    const [visits, setVisits] = useState(5000);
+
+    useEffect(() => {
+        fetchVisitsData().then(newVisits => {
+            setVisits(5000 + newVisits);
+        });
+    }, []);
+
     return (
         <div data-aos="fade-up">
-            <div className="aspect-square overflow-hidden transition duration-500 ease-in-out shadow-sm transform-gpu rounded-3xl mobile-hover:hover:scale-105 mobile-hover:hover:shadow-lg hover:rotate-0 hover:active:scale-105 hover:active:shadow-lg border-[0.5px] border-true-gray-100" dark="border-true-gray-900 border-none"
-            // data-aos="fade-up"
-            >
-                <div className="flex flex-row justify-between h-full bg-white shadow-sm p-3.5 " dark="bg-true-gray-900"
-                // data-aos="fade-up"         
-                >
+            <div className="aspect-square overflow-hidden transition duration-500 ease-in-out shadow-sm transform-gpu rounded-3xl mobile-hover:hover:scale-105 mobile-hover:hover:shadow-lg hover:rotate-0 hover:active:scale-105 hover:active:shadow-lg border-[0.5px] border-true-gray-100" dark="border-true-gray-900 border-none">
+                <div className="flex flex-row justify-between h-full bg-white shadow-sm p-3.5 " dark="bg-true-gray-900">
                     <div className="flex flex-col justify-between">
                         <div className="w-12 xs:text-[40px] animate-wave inline origin-bottom-right text-3xl">
                             👋
@@ -61,7 +79,7 @@ export const WidgetOverViewSmall: FC<{ posts: Post[], }> = ({ posts }) => {
                         <div className="xs:text-xl leading-4 xs:leading-6 font-semibold text-sm">
                             <p className={`${Colors["orange"]?.text.normal} line-clamp-1`}>{dateMap.length} 篇文章</p>
                             <p className={`${Colors["pink"]?.text.normal} line-clamp-1`}>{tagsAmount} 个话题</p>
-                            <OverviewPvAll />
+                            <OverviewPvAll visits={visits} />
                         </div>
                     </div>
                 </div>
@@ -97,16 +115,20 @@ export const WidgetOverViewMedium: FC<{ posts: Post[], fix?: boolean }> = ({ pos
     const postsDataset = previousMonthMapArray.map(p => p.count != 0 ? 1 : 0)
 
     const monthArray = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-    // const monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
     let todayMonth = new Date().getMonth();
     const monthLabel = [monthArray[todayMonth - 12 < 0 ? todayMonth : todayMonth - 12], "", "", "", "", "", "", "", "", "", "", monthArray[todayMonth - 6 < 0 ? todayMonth - 6 + 12 : todayMonth - 6], "", "", "", "", "", "", "", "", "", "", "", monthArray[todayMonth]]
 
     const { resolvedTheme } = useTheme()
     const [mounted, setMounted] = useState(false)
+    const [visits, setVisits] = useState(5000);
+
     useEffect(() => {
         setMounted(true)
-    }, [])
+        fetchVisitsData().then(newVisits => {
+            setVisits(5000 + newVisits);
+        });
+    }, []);
 
     if (!mounted) {
         return null
@@ -151,7 +173,6 @@ export const WidgetOverViewMedium: FC<{ posts: Post[], fix?: boolean }> = ({ pos
                     drawTicks: false,
                     drawBorder: false,
                     lineWidth: 1,
-                    // color: ["#bababa","#e5e5e5","#e5e5e5","#e5e5e5","#e5e5e5","#e5e5e5","#e5e5e5","#e5e5e5","#e5e5e5","#e5e5e5","#e5e5e5","#e5e5e5"]
                 },
                 ticks: {
                     padding: 5,
@@ -162,7 +183,6 @@ export const WidgetOverViewMedium: FC<{ posts: Post[], fix?: boolean }> = ({ pos
                     borderWidth: 10,
                     font: {
                         size: 7,
-                        // weight: "bold",
                         lineHeight: 1,
                     }
                 }
@@ -182,12 +202,8 @@ export const WidgetOverViewMedium: FC<{ posts: Post[], fix?: boolean }> = ({ pos
 
     return (
         <div data-aos="fade-up">
-            <div className={`overflow-hidden transition duration-500 ease-in-out shadow-sm transform-gpu ${fix ? "h-35 lg:h-40" : "h-40 lg:h-48"} rounded-3xl mobile-hover:hover:scale-105 mobile-hover:hover:shadow-lg hover:rotate-0 hover:active:scale-105 hover:active:shadow-lg border-[0.5px] border-true-gray-100`} dark="border-true-gray-900 border-none"
-            // data-aos="fade-up"
-            >
-                <div className="flex flex-row justify-between h-full bg-white shadow-sm px-3 py-2  lg:(px-4 py-3)" dark="bg-true-gray-900"
-                // data-aos="fade-up"
-                >
+            <div className={`overflow-hidden transition duration-500 ease-in-out shadow-sm transform-gpu ${fix ? "h-35 lg:h-40" : "h-40 lg:h-48"} rounded-3xl mobile-hover:hover:scale-105 mobile-hover:hover:shadow-lg hover:rotate-0 hover:active:scale-105 hover:active:shadow-lg border-[0.5px] border-true-gray-100`} dark="border-true-gray-900 border-none">
+                <div className="flex flex-row justify-between h-full bg-white shadow-sm px-3 py-2  lg:(px-4 py-3)" dark="bg-true-gray-900">
                     <div className="flex flex-col justify-between">
                         <div className={`text-4xl ${fix ? "" : "lg:text-5xl"} animate-wave inline origin-bottom-right w-12`}>
                             👋
@@ -195,7 +211,7 @@ export const WidgetOverViewMedium: FC<{ posts: Post[], fix?: boolean }> = ({ pos
                         <div className={`text-lg leading-6 md:leading-7  ${fix ? "" : "lg:text-2xl"} font-semibold`}>
                             <p className={`${Colors["orange"]?.text.normal}`}>{dateMap.length} 篇文章</p>
                             <p className={`${Colors["pink"]?.text.normal}`}>{tagsAmount} 个话题</p>
-                            <OverviewPvAll />
+                            <OverviewPvAll visits={visits} />
                         </div>
                     </div>
                     <div className="text-xs w-6/11 lg:(w-1/2 text-md) lg<:text-sm font-medium h-full flex flex-col justify-between">
@@ -247,11 +263,9 @@ const OverviewPv = () => {
     pv = pvData["pageHistory"]
 
     return (
-
         <Tooltip tooltipText={`⬇️${Math.min(...getTrimData(pv).last48)} ⬆️${Math.max(...getTrimData(pv).last48)} (周)`}>
             <BarChart data={pv} color="0, 122, 255" />
         </Tooltip>
-
     )
 }
 
@@ -286,14 +300,9 @@ const OverviewUv = () => {
     )
 }
 
-const OverviewPvAll = () => {
-    const { data: pvAllData, error: pvAllError } = useSWRImmutable('/api/page-views/all', fetcher)
-    if (pvAllError) return <p className={`${Colors["blue"]?.text.normal} line-clamp-1`}>25,223 次访问</p>
-    if (!pvAllData) return <p className={`${Colors["blue"]?.text.normal} animate-pulse line-clamp-1`}>- 次访问</p>
-    const pvAmount = pvAllData["pageViews"] || "-"
-
+const OverviewPvAll = ({ visits }) => {
     return (
-        <p className={`${Colors["blue"]?.text.normal} line-clamp-1`}>{pvAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 次访问</p>
+        <p className={`${Colors["blue"]?.text.normal} line-clamp-1`}>{visits.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 次访问</p>
     )
 }
 
@@ -336,7 +345,6 @@ const BarChart = ({ data, color }: { data: number[], color: string }) => {
     function barData(data: any, color: any): any {
         return (
             {
-                // labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
                 labels: monthLabel,
                 datasets: [
                     createDataset(getTrimData(data).odd, getTrimData(data, color).colors),
@@ -359,30 +367,18 @@ const BarChart = ({ data, color }: { data: number[], color: string }) => {
                     grid: {
                         drawBorder: false,
                         drawTicks: false,
-                        // drawOnChartArea: false,
-                        // color: ["white","white","white",],
-                        // color: ['#bababa','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#bababa','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','#e5e5e5','red'],
-                        // lineWidth: .1,
                     },
                     ticks: {
                         display: false,
-                        // callback : function(val, index) {
-                        //     // Hide every 2nd tick label
-                        //     return index % 3 === 0 ? this.getLabelForValue(val) : '';
-                        //   },
                         autoSkip: false,
                         maxRotation: 0,
                         font: {
                             size: 8,
-                            // weight: "bold",
                             lineHeight: 0.5,
-                            // padding: "-10px",
                         }
                     }
                 },
                 yAxis: {
-                    // position: 'right',
-
                     min: Math.min(...getTrimData(data).last48) - 5,
                     max: Math.max(...getTrimData(data).last48) + 5,
                     grid: {
