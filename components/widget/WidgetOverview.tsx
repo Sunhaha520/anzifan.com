6812147c028dea6918882f98fb26690aaea24c43
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-09-03 14:12:01 +0800

    Update WidgetOverview.tsx

--- components/widget/WidgetOverview.tsx
+++ components/widget/WidgetOverview.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState, useMemo } from "react";
 import Image from "next/image";
 import { links, LinkType } from "../../config/links";
 import {
@@ -86,118 +86,7 @@ export const WidgetOverViewSmall: FC<{ posts: Post[] }> = ({ posts }) => {
 };
 
 export const WidgetOverViewMedium: FC<{ posts: Post[], fix?: boolean }> = ({ posts, fix }) => {
-    const tagsMap = posts.map(p => ({ tags: p.tags, date: p.updateDate }))
-    const dateMap = posts.map(p => ({ date: new Date(p.updateDate) }))
-    const count = 0
-    const tagsAmount = tagsMap.reduce(
-        (prev, cur) => prev + cur.tags.length,
-        count
-    );
-
-    const [visitCount, setVisitCount] = useState(5000);
-
-    useEffect(() => {
-        setVisitCount(prevCount => prevCount + 1);
-    }, []);
-
-    const monthPosts = dateMap.map(d => `${d.date.getFullYear()}-${(d.date.getMonth()).toString()}-${(d.date.getDate()) <= 15 ? "0" : "1"}`);
-    const currentMonth = { year: new Date().getFullYear(), month: (new Date().getMonth()) }
-    let previousMonthMapArray = []
-    for (let i = 0; i < 12; ++i) {
-        const previousMonth = new Date(currentMonth.year, currentMonth.month - i)
-        previousMonthMapArray.push({ date: `${previousMonth.getFullYear()}-${(previousMonth.getMonth()).toString()}-1`, count: 0 })
-        previousMonthMapArray.push({ date: `${previousMonth.getFullYear()}-${(previousMonth.getMonth()).toString()}-0`, count: 0 })
-    }
-    previousMonthMapArray.reverse().map(post => {
-        monthPosts.filter(p => {
-            if (p === post.date) {
-                post.count += 1
-            }
-        })
-    })
-
-    const postsDataset = previousMonthMapArray.map(p => p.count != 0 ? 1 : 0)
-
-    const monthArray = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-
-    let todayMonth = new Date().getMonth();
-    const monthLabel = [monthArray[todayMonth - 12 < 0 ? todayMonth : todayMonth - 12], "", "", "", "", "", "", "", "", "", "", monthArray[todayMonth - 6 < 0 ? todayMonth - 6 + 12 : todayMonth - 6], "", "", "", "", "", "", "", "", "", "", "", monthArray[todayMonth]]
-
-    const { resolvedTheme } = useTheme()
-    const [mounted, setMounted] = useState(false)
-    useEffect(() => {
-        setMounted(true)
-    }, [])
-
-    if (!mounted) {
-        return null
-    }
-
-    const ticksColor = monthLabel.map((label, index) =>
-        parseInt(label) >= todayMonth + 1 && index != 23 ? resolvedTheme === "dark" ? "#434343":"#bababa" : resolvedTheme === "dark" ? "#ffffff":"#000000"
-    )
-
-    const postsData: any = {
-        labels: monthLabel,
-        datasets: [
-            {
-                data: postsDataset,
-                borderRadius: Number.MAX_VALUE,
-                borderSkipped: false,
-                barPercentage: 1,
-                gradient: {
-                    backgroundColor: {
-                        axis: 'y',
-                        colors: {
-                            0: 'rgba(255, 149, 0, 1)',
-                            100: 'rgba(255, 149, 0, 0.5)',
-                        }
-                    },
-                }
-            }
-        ]
-    }
-
-    const postsOptions: any = {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            xAxes: {
-                afterFit: (axis: any) => {
-                    axis.paddingRight = 1;
-                    axis.paddingLeft = 1;
-                    axis.paddingTop = 6.5;
-                },
-                grid: {
-                    drawTicks: false,
-                    drawBorder: false,
-                    lineWidth: 1,
-                },
-                ticks: {
-                    padding: 5,
-                    display: true,
-                    autoSkip: false,
-                    maxRotation: 0,
-                    color: ticksColor,
-                    borderWidth: 10,
-                    font: {
-                        size: 7,
-                        lineHeight: 1,
-                    }
-                }
-            },
-            yAxes: {
-                grid: {
-                    drawOnChartArea: false,
-                    drawTicks: false,
-                    drawBorder: false,
-                },
-                ticks: {
-                    display: false,
-                }
-            }
-        }
-    }
+    const { tagsAmount, dateMap, visitCount } = useWidgetData(posts);
 
     return (
         <div data-aos="fade-up">
@@ -224,61 +113,85 @@ export const WidgetOverViewMedium: FC<{ posts: Post[], fix?: boolean }> = ({ pos
                             <p className="mb-2">访问</p>
                             <OverviewPv />
                         </div>
-                        <div>
-                            <p>文章</p>
-                            <div className="h-6.8 md:h-6.6 lg:h-7.3" >
-                                <Bar data={postsData} options={postsOptions} />
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 const OverviewPv = () => {
     const [visitCount, setVisitCount] = useState(5000);
 
     useEffect(() => {
-        setVisitCount(prevCount => prevCount + 1);
+        const script = document.createElement('script');
+        script.src = "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js";
+        script.async = true;
+        document.body.appendChild(script);
+
+        script.onload = () => {
+            const busuanziValue = document.getElementById('busuanzi_value_site_pv');
+            if (busuanziValue) {
+                const busuanziCount = parseInt(busuanziValue.innerText, 10);
+                setVisitCount(prevCount => prevCount + busuanziCount);
+            }
+        };
+
+        return () => {
+            document.body.removeChild(script);
+        };
     }, []);
 
-    let pv = [9, 179, 78, 171, 109, 51, 97, 71, 59, 39, 41, 39, 60, 44, 65, 51, 80, 60, 97, 153, 4, 4, 42, 26, 72, 40, 92, 16, 21, 26, 38, 34, 43, 23, 30, 40, 21, 14, 74, 32, 46, 35, 84, 69, 45, 25, 85, 84, 85, 46, 53, 156, 62]
+    let pv = [9, 179, 78, 171, 109, 51, 97, 71, 59, 39, 41, 39, 60, 44, 65, 51, 80, 60, 97, 153, 4, 4, 42, 26, 72, 40, 92, 16, 21, 26, 38, 34, 43, 23, 30, 40, 21, 14, 74, 32, 46, 35, 84, 69, 45, 25, 85, 84, 85, 46, 53, 156, 62];
 
     return (
         <Tooltip tooltipText={`⬇️${Math.min(...getTrimData(pv).last48)} ⬆️${Math.max(...getTrimData(pv).last48)} (周)`}>
             <BarChart data={pv} color="0, 122, 255" />
         </Tooltip>
-    )
-}
+    );
+};
 
 const OverviewUv = () => {
     const [visitCount, setVisitCount] = useState(5000);
 
     useEffect(() => {
-        setVisitCount(prevCount => prevCount + 1);
+        const script = document.createElement('script');
+        script.src = "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js";
+        script.async = true;
+        document.body.appendChild(script);
+
+        script.onload = () => {
+            const busuanziValue = document.getElementById('busuanzi_value_site_pv');
+            if (busuanziValue) {
+                const busuanziCount = parseInt(busuanziValue.innerText, 10);
+                setVisitCount(prevCount => prevCount + busuanziCount);
+            }
+        };
+
+        return () => {
+            document.body.removeChild(script);
+        };
     }, []);
 
-    let uv = [8, 52, 45, 51, 54, 34, 46, 35, 37, 29, 34, 33, 36, 40, 51, 39, 50, 33, 53, 23, 4, 3, 16, 22, 32, 27, 31, 14, 12, 21, 15, 18, 18, 18, 15, 25, 13, 13, 25, 14, 25, 23, 44, 42, 28, 16, 26, 47, 58, 43, 36, 45, 36]
+    let uv = [8, 52, 45, 51, 54, 34, 46, 35, 37, 29, 34, 33, 36, 40, 51, 39, 50, 33, 53, 23, 4, 3, 16, 22, 32, 27, 31, 14, 12, 21, 15, 18, 18, 18, 15, 25, 13, 13, 25, 14, 25, 23, 44, 42, 28, 16, 26, 47, 58, 43, 36, 45, 36];
 
     return (
         <Tooltip tooltipText={`⬇️${Math.min(...getTrimData(uv).last48)} ⬆️${Math.max(...getTrimData(uv).last48)} (周)`}>
             <BarChart data={uv} color="255, 45, 85" />
         </Tooltip>
-    )
-}
+    );
+};
 
 function getTrimData(originalData: Array<number>, color?: any) {
-    const last48 = originalData.slice(Math.max(originalData.length - 48, 0))
-    const lowest = Math.max(...last48) / 7
-    const trimLast48 = last48.map((v: any) => v > lowest ? v : lowest)
-    const odd = trimLast48.filter((c: any, i: number) => i % 2)
-    const even = trimLast48.filter((c: any, i: number) => !(i % 2))
-    let colors: any = { 0: `rgba(${color}, 0.8)` }
-    colors[Math.max(...last48)] = `rgba(${color}, 0.4)`
+    const last48 = originalData.slice(Math.max(originalData.length - 48, 0));
+    const lowest = Math.max(...last48) / 7;
+    const trimLast48 = last48.map((v: any) => v > lowest ? v : lowest);
+    const odd = trimLast48.filter((c: any, i: number) => i % 2);
+    const even = trimLast48.filter((c: any, i: number) => !(i % 2));
+    let colors: any = { 0: `rgba(${color}, 0.8)` };
+    colors[Math.max(...last48)] = `rgba(${color}, 0.4)`;
 
-    return { odd, even, colors, last48 }
+    return { odd, even, colors, last48 };
 }
 
 function createDataset(data: Array<number>, color: any) {
@@ -295,15 +208,14 @@ function createDataset(data: Array<number>, color: any) {
                 colors: color
             },
         }
-    }
+    };
 }
 
 const BarChart = ({ data, color }: { data: number[], color: string }) => {
-
-    const monthArray = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+    const monthArray = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
     let todayMonth = new Date().getMonth();
 
-    const monthLabel = [monthArray[todayMonth - 12 < 0 ? todayMonth : todayMonth - 12], "", "", "", "", "", "", "", "", "", "", monthArray[todayMonth - 6 < 0 ? todayMonth - 6 + 12 : todayMonth - 6], "", "", "", "", "", "", "", "", "", "", "", monthArray[todayMonth]]
+    const monthLabel = [monthArray[todayMonth - 12 < 0 ? todayMonth : todayMonth - 12], "", "", "", "", "", "", "", "", "", "", monthArray[todayMonth - 6 < 0 ? todayMonth - 6 + 12 : todayMonth - 6], "", "", "", "", "", "", "", "", "", "", "", monthArray[todayMonth]];
 
     function barData(data: any, color: any): any {
         return (
@@ -314,7 +226,7 @@ const BarChart = ({ data, color }: { data: number[], color: string }) => {
                     createDataset(getTrimData(data).even, getTrimData(data, color).colors),
                 ],
             }
-        )
+        );
     };
 
     function barOptions(data: any): any {
@@ -342,7 +254,7 @@ const BarChart = ({ data, color }: { data: number[], color: string }) => {
                     }
                 },
                 yAxis: {
-                    min: Math.min(...getTrimData(data).last48)-5,
+                    min: Math.min(...getTrimData(data).last48) - 5,
                     max: Math.max(...getTrimData(data).last48) + 5,
                     grid: {
                         drawOnChartArea: false,
@@ -358,8 +270,9 @@ const BarChart = ({ data, color }: { data: number[], color: string }) => {
                     }
                 }
             }
-        })
+        });
     }
-    return <Bar className="" data={barData(data, color)} options={barOptions(data)} width="100%" height="12" />
-}
-                                  
+    return <Bar className="" data={barData(data, color)} options={barOptions(data)} width="100%" height="12" />;
+};
+
+export default { WidgetOverViewSmall, WidgetOverViewMedium, OverviewPv, OverviewUv, BarChart };
